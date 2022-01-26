996d98ac22b3954223280cda858dc9a261b7764a
rehab hamdy <rehab93hamdy@gmail.com>
2022-01-26 23:29:59 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+
+    $("#about").click(function(){ 
+        $("body").animate({
+                scrollTop : $(".about").offset().top
+            },1000);
+    })
+    
+    
+    $("#services").click(function(){ 
+        $("body").animate({
+                scrollTop : $(".Our_Services").offset().top
+            },1000);
+    })
+
+
+    $("#portfolio").click(function(){ 
+        $("body").animate({
+                scrollTop : $(".Our_Portfolio").offset().top
+            },1000);
+    })
+
+
+    $("#testimonial").click(function(){ 
+        $("body").animate({
+                scrollTop : $(".testimonial").offset().top
+            },1000);
+    })
+
+
+
+    $("#blog").click(function(){ 
+        $("body").animate({
+                scrollTop : $(".Our_Blog").offset().top
+            },1000);
+    })
+
+
+    $("#contact").click(function(){ 
+        $("body").animate({
+                scrollTop : $(".Contact_us").offset().top
+            },1000);
+    })
+
+
+
+
+
+    
+
+
+
+
+
+
